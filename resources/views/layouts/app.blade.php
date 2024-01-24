bcb81723d496657bdd5cc022796676ee912a5ea3
na0260 <samjr.008@gmail.com>
2024-01-25 00:03:13 +0600

    mod

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,8 +27,6 @@
 </script>
 <script src="{{ asset('/') }}backend-assets/js/scripts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-<script src="{{ asset('/') }}backend-assets/assets/demo/chart-area-demo.js"></script>
-<script src="{{ asset('/') }}backend-assets/assets/demo/chart-bar-demo.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
 <script src="{{ asset('/') }}backend-assets/js/datatables-simple-demo.js"></script>
 </body>
